3553437d66997883e2130f4c2937fe4b325bf5c5
Hiper Tecnologia <hiper@hiper-tecnologia.com>
2019-03-16 23:58:34 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,11 +3,12 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORT ATUALIZADO
 import { NON_TEXT_INPUT_REGEX } from 'ionic-angular/umd/util/dom';
 import { StorageService } from '../services/storage.services';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
     
-    constructor(public storage: StorageService){
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
 
     }
 
@@ -29,9 +30,17 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
             
             switch(errorObj.status) {
+                
+                case 401:
+                this.handle401();
+                break;
+
                 case 403:
                 this.handle403();
                 break;
+
+                default:
+                this.handleDefaultError(errorObj);
             }
 
             return Observable.throw(errorObj);
@@ -40,6 +49,32 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Login ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]
+
+        });
+        alert.present();
+    }
+
+    handleDefaultError(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro '+ errorObj.status + ': '+ errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]
+
+        });
+        alert.present();
     }
 
 }
--- src/services/domain/funcionario.service.ts
+++ src/services/domain/funcionario.service.ts
@@ -27,4 +27,3 @@ export class FuncionarioService {
             `${API_CONFIG.baseUrl}/funcionarios/login?value=${login}`)};//,
             //{'headers': authHeader});
     }
-}
